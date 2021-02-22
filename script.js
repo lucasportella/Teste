b537a7053e56fb2151428070e1ba98025a2b3fb8
lucasportella <lucasportellaagu@gmail.com>
2021-02-22 16:20:15 -0300

    js beginning

---
+++ script.js
@@ -0,0 +1,15 @@
+let trafficLight = "dwada";
+
+switch (trafficLight) {
+    case "vermelho":
+        console.log("pare");
+        break;
+    case "amarelo":
+        console.log("cuidado");
+        break;
+    case "verde":
+        console.log("siga");
+        break;
+    default:
+        console.log("valor não identificado")
+}
